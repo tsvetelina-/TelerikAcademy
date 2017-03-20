2eb97b8ff98d817a481ba497d63994d4c517e713
tsvetelina- <tsvetelina.a.marinova@gmail.com>
2017-03-20 13:39:39 +0200

    JavaScript Fundamentals

---
+++ JavaScript-Fundamentals/1-Data-Types-And-Variables/01.JavaScriptLiterals.js
@@ -0,0 +1,14 @@
+function solve() {
+    let integer = 45,
+        floatingPoint = 3.1415,
+        booleanType = true,
+        string = 'this is of type string',
+        char = 'a';
+
+    console.log(integer);
+    console.log(floatingPoint);
+    console.log(booleanType);
+    console.log(string);
+    console.log(char);
+}
+solve();
